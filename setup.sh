4c45d02e987e9a545543c826558cd38dc9fe8a0f
Scott Francis <scott.russell.francis@gmail.com>
2018-10-21 10:02:04 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "!! Do these steps manually"; echo ""
+echo "sudo rpi-update"
+echo "sudo reboot"
+echo "-----------"
+echo "sudo apt update && sudo apt upgrade -y"
+
+sudo apt install git,python-pip -y
